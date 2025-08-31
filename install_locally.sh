d348dd9fcf2dcc4e63a2f27b487724703981e406
Kate <alicelegare@outlook.com>
2025-08-31 12:55:16 -0400

    orsiteanrsoiteanr

--- install_locally.sh
+++ install_locally.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/bash
 
-directory="$HOME/.typst/packages/local/bigsets/0.1.0"
+directory="$HOME/.local/share/typst/packages/local/bigsets/0.1.0"
 
 mkdir -p "$directory"
+rm "$directory"/* -f
 cp ./* "$directory" -r
